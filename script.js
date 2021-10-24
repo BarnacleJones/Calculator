6f145efde9705acfd254ca4d66d657107daff8e3
Ben <benmjones@protonmail.com>
2021-10-25 11:34:30 +1300

    GUI and first functions

---
+++ script.js
@@ -0,0 +1,14 @@
+//Basic math functions
+
+const add = (a, b) => a+b;
+
+const subtract = (a, b) => a-b;
+
+const multiply = (a, b) => a*b;
+
+const divide = (a, b) => a/b;
+
+//function to call the above, with two numbers 
+
+const operate = (operator, num1, num2) => operator(num1, num2);
+
